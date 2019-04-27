b41c2704a969b2490abdc38851e7d22ee4bf7042
dreamdky0407 <50054111+dreamdky0407@users.noreply.github.com>
2019-04-28 00:57:55 +0800

    dreamsky
    add abc

---
+++ abc/Assets/movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public Animator animator;
+
+     float InputX;
+    public float InputY;
+    // Use this for initialization
+    void Start () {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InputY = Input.GetAxis("Vertical");
+        InputX = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputY", InputY);
+        animator.SetFloat("InputX", InputX);
+
+    }
+}
